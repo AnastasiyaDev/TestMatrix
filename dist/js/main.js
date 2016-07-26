2a3c4d2d3992dddd9c150a0401a7c78c21887aae
Anastasiya Bakhnar <katana8832@yandex.ru>
2016-07-26 19:45:31 +0300

    fix footer

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
 	    {
 	      breakpoint: 1024,
 	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
+	        slidesToShow: 2,
+	        slidesToScroll: 1,
 	        infinite: true,
 	        dots: false
 	      }
@@ -29,8 +29,8 @@ $(document).ready(function(){
 	    {
 	      breakpoint: 1024,
 	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
+	        slidesToShow: 2,
+	        slidesToScroll: 1,
 	        infinite: true,
 	        dots: false
 	      }
@@ -48,8 +48,8 @@ $(document).ready(function(){
 	    {
 	      breakpoint: 1024,
 	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
+	        slidesToShow: 2,
+	        slidesToScroll: 1,
 	        infinite: true,
 	        dots: false
 	      }
