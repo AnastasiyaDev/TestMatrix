695fb305e726898e718ec1010123d6429e545295
Anastasiya Bakhnar <katana8832@yandex.ru>
2016-07-26 21:28:28 +0300

    add fancybox/video in front

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('.steps-videos__slider').slick({
+  $('.slider').slick({
 	  dots: false,
 	  infinite: true,
 	  speed: 300,
@@ -19,51 +19,42 @@ $(document).ready(function(){
 	  ]
   });
 
- $('.education-videos__slider').slick({
-	  dots: false,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 1,
-	        infinite: true,
-	        dots: false
-	      }
-	    }
-	  ]
-  });
+// helpers : {
+// 			media : {
+// 			    youtube : {
+// 			         params : {
+// 			             theme : 'light',
+//                          vq    : 'hd720',
+//                          css   : {
+//                             'body' : 'color: #fff'
+//                          } 
+// 			         }
+// 			    } 
+// 			}
+// 		}
 
-  $('.inspiration-videos__slider').slick({
-	  dots: false,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 1,
-	        infinite: true,
-	        dots: false
-	      }
-	    }
-	  ]
-  });
+  $(".youtube-link").click(function() {
+	$.fancybox({
+			'padding'		: 0,
+			'autoScale'		: true,
+			'transitionIn'	: 'none',
+			'transitionOut'	: 'none',
+			'title'			: this.title,
+			'maxWidth'      : '800',
+      		'maxHeight'     : '500',
+      		'width'         : '70%',
+      		'height'        : '50%',
+			'href'			: this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+			'type'			: 'swf',
+			'swf'			: {
+			   	 'wmode'		: 'transparent',
+				'allowfullscreen'	: 'true'
+			}
 
+		});
 
-//   $('.steps-videos__slider').slick({
-//   dots: true,
-//   infinite: true,
-//   speed: 300,
-//   slidesToShow: 1,
-//   adaptiveHeight: true
-// });
+	return false;
+});
 
+ 
 });
